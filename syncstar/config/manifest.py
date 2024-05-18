2f1072c64806f25dbd47ac37b1d50d03ac46bbdd
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-05-18 17:12:27 +0530

    Add manifest for controlled supply of static images
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ syncstar/config/manifest.py
@@ -0,0 +1,47 @@
+"""
+SyncStar
+Copyright (C) 2024 Akashdeep Dhar
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Any Red Hat trademarks that are incorporated in the source code or
+documentation are not subject to the GNU General Public License and may only
+be used or replicated with the express permission of Red Hat, Inc.
+"""
+
+
+icondict = {
+    "gdroid": "9d52ca1ee37208482a05038d59664689d32f53f5189486932db8d6adbe481126.svg",
+    "archlx": "1c48b14227a7e1091e740a3df7174bbddc453917cc287e503315ae6f1a845b0e.svg",
+    "centos": "87e960134335e7a8888b69331157a8a84215ed8dbb041ecd3ce73d3866d7f3d3.svg",
+    "debian": "f981aab3aaa188a7d289009af14600152c1ea28e9ad758ed4b97aa5620a74456.svg",
+    "fedora": "e56f0c223307c5ebc30f78872f1887189715a2afd18c9e9204a63c03e781c271.svg",
+    "kodimc": "2ad165fc2120ad592bdbf852fe501ddaad6e2b944bff7d945d127b4e73a1699e.svg",
+    "lxmint": "33f5c702ffcee2b326dec6d9e8b46730a8cf975a66fc0e5d84e674cacf41c3e4.svg",
+    "common": "fdf8c530789c78a450d5fae444905349afad1c6c257fb40b6a80de3fa7565c03.svg",
+    "mnjaro": "246f21c66f18e728feb8e9ad0dd8f35b116656ddb519819e13798a579bdbc59a.svg",
+    "redhat": "170d2033c590e5e7b694ea5f7ae047fbf561768c78e30bf0f545f66e164ebc18.svg",
+    "opsuse": "e31d78dec9f573dac4e9fda28dd0d3357f4a3b7f5361f7be2498f223310f7254.svg",
+    "ubuntu": "3b7f5a60779863249634141d594548e56208889dc32b783142cbf3999e8adbda.svg",
+    "device": "72c09374228d729fe5b46fb215149f1da1052f64b768bba12fba27679fa902fa.svg",
+    "result": "13bfa57f52e6818e7ca1b403fc2fc9f40dfd9276aaa44729b0f9fe2790e1a69d.svg",
+    "verify": "deef37d937f28061f6a8a453bc7660951b32a34c23d0975c71ada8b4ea399f52.svg",
+    "fiasco": "385b4204a81711016e4360def38f14b317bf0341a9b85a96de6e94cbf9be4c85.svg",
+    "finish": "4c146bdd196418efd6ce5db791243ae6dd3527309663954e565405de0d70afaf.svg",
+    "create": "ca6ff012f78ea3989e5da13395102c7289cbe6858c0a7eea6cf82970b017ab24.svg",
+    "canvas": "a50f6a86f40bae105722f35209e70ef6e37ef1a11c6e98a39c70114c7f3b07c0.svg",
+    "detail": "4b08662a1e2bd5e674c920cb1a7fb226d11f1199a6de17ee3ac8431849af150f.svg",
+    "lesson": "c2bbab4baf8aebcb3c054b9b6c28e2340a3b42064f72f126c49962affca34a56.svg",
+    "bounce": "7fe596ee4ec3b5731f80248519d95bc93a620304b2f260c64a85d392cf90ac6a.svg",
+}
